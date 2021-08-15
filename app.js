d8714ef02df16d195d1e54d09071db437e58a004
Simran Gangwani <simrangngwani3107@gmail.com>
2021-08-16 02:27:46 +0530

    style: added graphics

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var after_submit = document.querySelector("#after_submit");
 
 btn.addEventListener("click", () => {
 
+    document.getElementById('graph').style.display="none";
     var purchase = Number(purc.value);
     console.log(purchase);
     var quantity = Number(quan.value);
@@ -26,18 +27,26 @@ btn.addEventListener("click", () => {
         {
             document.getElementById('after_submit').style.display="block";
             after_submit.textContent = "No profit no loss";
+            document.getElementById('ok').style.display="block";
+            document.getElementById('profit').style.display="none";
+            document.getElementById('loss').style.display="none";
         }
         else if (diff > 0)
         {
             var profitPer = ((diff / totalPurchase) * 100).toFixed(2);
             document.getElementById('after_submit').style.display="block";
             document.getElementById('after_submit').innerHTML = `Congrats!! You got a profit of Rs ${diff} which is ` + profitPer + `%`;
-            document.getElementById('resultImg').innerHTML = "<img src='/images/pic.png' width='16' height='16'>"
+            document.getElementById('profit').style.display="block";
+            document.getElementById('loss').style.display="none";
+            document.getElementById('ok').style.display="none";
         }
         else{
             var lossPer = ((-diff / totalPurchase) * 100).toFixed(2);
             document.getElementById('after_submit').style.display="block";
             document.getElementById('after_submit').innerHTML = `Awww, you are in loss of Rs ${-diff} which is ` + lossPer + `%`;
+            document.getElementById('loss').style.display="block";
+            document.getElementById('profit').style.display="none";
+            document.getElementById('ok').style.display="none";
         }
     }
 });
